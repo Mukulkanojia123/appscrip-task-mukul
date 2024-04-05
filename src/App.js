17c5d545eed87dca28a4823c129b350c52c166ce
Mukulkanojia123 <mkanojia1996@gmail.com>
2024-04-06 00:15:33 +0530

    adding footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import MainHeader from './header/MainHeader';
 import Body from './Body/Body';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div >
       <MainHeader />
       <Body />
+      <Footer />
     </div>
   );
 }
